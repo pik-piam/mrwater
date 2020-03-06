f393ff48c1a0379397917ff77f03a400be94739e
FeBeier <felicitas.beier@pik-potsdam.de>
2020-03-06 11:26:11 +0100

    added CalcIrrigation

---
+++ R/calcIrrigation.R
@@ -0,0 +1,42 @@
+#' @title calcIrrigation
+#' @description This function extracts irrigation water (airrig: water applied additionally to rainfall) from LPJmL to MAgPIE
+#'
+#' @return magpie object in cellular resolution
+#' @author Felicitas Beier, Abhijeet Mishra
+#'
+#' @examples
+#' \dontrun{ calcOutput("Irrigation", aggregate = FALSE) }
+#'
+#' @import madrat
+#' @import magclass
+#' @import moinput
+#' @importFrom magpiesets findset
+
+calcIrrigation <- function(years = seq(1995,2095,by=5)){
+
+  # Read in airrig:
+  lpj_airrig <- readSource("LPJmL5", subtype="CRU_4.irrig",convert="onlycorrect")
+    # Note: airrig: irrigation water applied additionally to rainfall
+    # rainfed=0
+
+  # Load LPJmL to MAgPIE mapping to aggregate to MAgPIE crops
+  LPJ2MAG      <- toolGetMapping( "MAgPIE_LPJmL.csv", type = "sectoral", where = "mappingfolder")
+
+  # Aggregate to MAgPIE crops
+  mag_airrig   <- toolAggregate(lpj_airrig, LPJ2MAG, from = "LPJmL", to = "MAgPIE", dim=3.1, partrel=TRUE)
+
+  # Remove negative airrig
+  mag_airrig[mag_airrig < 0] <- 0
+
+  # Check for NAs
+  if(any(is.na(mag_airrig))){
+    stop("produced NA airrig")
+  }
+
+  return(list(
+    x=mag_airrig,
+    weight=NULL,
+    unit="m^3 per ha per yr",
+    description="Irrigation water (water applied in addition to rainfall) in m^3 per hectar per year for different crop types following LPJmL irrigation system assumptions.",
+    isocountries=FALSE))
+}
