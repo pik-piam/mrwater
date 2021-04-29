2707aa2b66cd7c20028369ce42d1813719e96b30
Felicitas <beier@pik-potsdam.de>
2021-04-29 10:26:34 +0200

    included option of FAO calibrated yields in calcIrrigYieldImprovementPotential

--- R/calcIrrigYieldImprovementPotential.R
+++ R/calcIrrigYieldImprovementPotential.R
@@ -1,11 +1,12 @@
 #' @title       calcIrrigYieldImprovementPotential
 #' @description This function calculates the yield improvement potential of irrigation for different crops
 #'
-#' @param climatetype Switch between different climate scenarios or historical baseline "GSWP3-W5E5:historical" for yields
-#' @param selectyears years to be returned by the function
-#' @param monetary        yield improvement potential in tDM (FALSE, default) or priced yield improvement potential in USD05 (TRUE)
-#' @param iniyear         year to be used when monetary activated
-#' @param proxycrop   proxycrop(s) selected for crop mix specific calculations: average over proxycrop(s) yield gain. NULL returns all crops individually
+#' @param climatetype   Switch between different climate scenarios or historical baseline "GSWP3-W5E5:historical" for yields
+#' @param selectyears   years to be returned by the function
+#' @param monetary      yield improvement potential in tDM (FALSE, default) or priced yield improvement potential in USD05 (TRUE)
+#' @param iniyear       year to be used when monetary activated
+#' @param proxycrop     proxycrop(s) selected for crop mix specific calculations: average over proxycrop(s) yield gain. NULL returns all crops individually
+#' @param FAOyieldcalib TRUE (LPJmL yields scaled with current FAO yield) or FALSE (LPJmL yield potentials)
 #'
 #' @return magpie object in cellular resolution
 #' @author Felicitas Beier
@@ -14,11 +15,11 @@
 #' \dontrun{ calcOutput("IrrigYieldImprovementPotential", aggregate=FALSE) }
 #'
 #' @importFrom madrat calcOutput toolGetMapping
-#' @importFrom magclass collapseNames new.magpie getYears getNames collapseDim
+#' @importFrom magclass collapseNames new.magpie getYears getNames collapseDim add_columns getCells
 #' @importFrom magpiesets addLocation
 #' @importFrom mrcommons toolGetMappingCoord2Country
 
-calcIrrigYieldImprovementPotential <- function(climatetype, monetary, iniyear, selectyears, proxycrop) {
+calcIrrigYieldImprovementPotential <- function(climatetype, monetary, iniyear, selectyears, proxycrop, FAOyieldcalib) {
 
   # read in yields [in tons/ha]
   yields     <- calcOutput("Yields", lpjml=c(natveg="LPJmL4_for_MAgPIE_84a69edd", crop="ggcmi_phase3_nchecks_72c185fa"), cells="lpjcell", climatetype=climatetype, years=selectyears, aggregate=FALSE)
@@ -26,6 +27,29 @@ calcIrrigYieldImprovementPotential <- function(climatetype, monetary, iniyear, s
   # yield gap (irrigated vs. rainfed) [in tons/ha]
   yield_gain <- collapseNames(yields[,,"irrigated"]) - collapseNames(yields[,,"rainfed"])
   # (Note: irrigation may lead to shift in growing period -> tmp can have negative values; also: under N-stress, irrigation may lead to lower yields, the latter is only relevant for limited-N-LPJmL version, default: unlimited N)
+
+  if (FAOyieldcalib) {
+    FAOproduction <- setYears(collapseNames(calcOutput("FAOmassbalance_pre", aggregate=FALSE)[,,"production"][,iniyear,"dm"]), NULL)
+    LUHcroparea   <- setYears(calcOutput("Croparea", sectoral="kcr", physical=TRUE, aggregate=FALSE)[,iniyear,], NULL)
+
+    # calculate country FAO yields
+    croplist      <- intersect(getNames(FAOproduction), getNames(LUHcroparea))
+    FAOyields     <- ifelse(LUHcroparea[,,croplist]>0, FAOproduction[,,croplist] / LUHcroparea[,,croplist], 0)
+    # global mean
+    FAOyieldsGLO  <- dimSums(FAOyields, dim=1) / length(getCells(FAOyields))
+
+    # calibration factor
+    Calib         <- FAOyields / FAOyieldsGLO
+
+    # add crops that are missing in FAO
+    missingcrops  <- setdiff(getNames(LUHcroparea), getNames(FAOproduction))
+    Calib         <- add_columns(Calib, addnm=missingcrops, dim=3.1)
+    Calib[,,missingcrops]     <- 0
+    names(dimnames(Calib))[1] <- "iso"
+
+    croplist      <- intersect(getNames(Calib), getNames(yield_gain))
+    yield_gain    <- yield_gain[,,croplist] * Calib[intersect(unique(gsub(".*\\.", "", getCells(yield_gain))), getCells(Calib)),,croplist]
+  }
 
   if (monetary) {
     # Read in crop output price in initialization (USD05/tDM)
@@ -56,13 +80,14 @@ calcIrrigYieldImprovementPotential <- function(climatetype, monetary, iniyear, s
       # historical share of crop types in total cropland per cell
       croparea_shr <- croparea / dimSums(croparea, dim=3)
       # correct NAs: where no current cropland available -> representative crops (maize, rapeseed, pulses) assumed as proxy
-      rep_crops   <- c("maiz", "rapeseed", "puls_pro")
-      other_crops <- setdiff(getNames(croparea), rep_crops)
+      rep_crops    <- c("maiz", "rapeseed", "puls_pro")
+      other_crops  <- setdiff(getNames(croparea), rep_crops)
       croparea_shr[,,rep_crops][dimSums(croparea, dim=3)==0]   <- 1 / length(c("maiz", "rapeseed", "puls_pro"))
       croparea_shr[,,other_crops][dimSums(croparea, dim=3)==0] <- 0
 
-      # average yield gain over histrical crops weighted with their croparea share
-      yield_gain  <- dimSums(croparea_shr * yield_gain, dim=3)
+      # average yield gain over hisotrical crops weighted with their croparea share
+      croplist    <- intersect(getNames(yield_gain), getNames(croparea_shr))
+      yield_gain  <- dimSums(croparea_shr[,,croplist] * yield_gain[,,croplist], dim=3)
 
       description <- "Average yield improvement potential for crop types weighted with historical croparea share"
 
