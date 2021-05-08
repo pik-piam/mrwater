1e27d26bb39f4eb71dba4b44037d819e9e9a8ad5
Felicitas <beier@pik-potsdam.de>
2021-05-08 10:04:23 +0200

    new FAO yield calibration method. NOTE: STILL NEEDS BUGFIXING

--- R/calcIrrigYieldImprovementPotential.R
+++ R/calcIrrigYieldImprovementPotential.R
@@ -16,16 +16,20 @@
 #' \dontrun{ calcOutput("IrrigYieldImprovementPotential", aggregate=FALSE) }
 #'
 #' @importFrom madrat calcOutput toolGetMapping
-#' @importFrom magclass collapseNames new.magpie getYears getNames collapseDim add_columns getCells
+#' @importFrom magclass collapseNames new.magpie getYears getNames collapseDim add_columns getCells getSets
 #' @importFrom magpiesets addLocation
 #' @importFrom mrcommons toolGetMappingCoord2Country
 
 calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, monetary, iniyear, selectyears, proxycrop, FAOyieldcalib) {
 
-  # read in yields [in tons/ha]
+  # read in cellular lpjml yields [in tons/ha]
   yields     <- setYears(calcOutput("Yields", lpjml=lpjml, cells="lpjcell", climatetype=climatetype, years=selectyears, aggregate=FALSE), selectyears)
   # only crops (pasture is not irrigated)
   yields     <- yields[,,"pasture",invert=T]
+  # set correct dimension names
+  getSets(yields)[c("d3.1", "d3.2")] <- c("MAG", "irrigation")
+  #*#*#*# @KRISTINE: Glaubst du, es kann potenziell ein Problem sein, dass yields MAG.irrigation ist und croparea irrigation.MAG oder sind die MAgPIE-Objekte dafür ausreichend fool-proof? #*#*#*ä
+  #*#*#*# @KRISTINE: Ist das mit dem getSets() ok so, oder hätte ich da potenziell das Problem, dass sich die Funktion ändert und das mit d3.1 d3.2 nicht mehr zusammen passt bzw. sich die Anzahl der Dimenstionen in calcYields ändert o.Ä.?
 
   # yield gap (irrigated vs. rainfed) [in tons/ha]
   yield_gain <- collapseNames(yields[,,"irrigated"]) - collapseNames(yields[,,"rainfed"])
@@ -34,26 +38,58 @@ calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, monetary, ini
   # magpie crops
   croplist   <- getNames(yield_gain)
 
+  if (FAOyieldcalib | !is.null(proxycrop) && length(proxycrop)==1 && proxycrop=="historical") {
+    # read in total (irrigated + rainfed) croparea
+    croparea <- calcOutput("Croparea", years=iniyear, sectoral="kcr", cells="lpjcell", physical=TRUE, cellular=TRUE, irrigation=TRUE, aggregate=FALSE)
+    #### adjust cell name (until 67k cell names fully integrated in calcCroparea and calcLUH2v2!!!) ####
+    map                          <- toolGetMappingCoord2Country()
+    getCells(croparea)           <- paste(map$coords, map$iso, sep=".")
+    names(dimnames(croparea))[1] <- "x.y.iso"
+    #### adjust cell name (until 67k cell names fully integrated in calcCroparea and calcLUH2v2!!!) ####
+  }
+
   if (FAOyieldcalib) {
-    FAOproduction <- setYears(collapseNames(calcOutput("FAOmassbalance_pre", aggregate=FALSE)[,,"production"][,iniyear,"dm"]), NULL)
-    LUHcroparea   <- setYears(calcOutput("Croparea", sectoral="kcr", physical=TRUE, aggregate=FALSE)[,iniyear,], NULL)
 
-    # calculate country FAO yields
-    croplist      <- intersect(getNames(FAOproduction), getNames(LUHcroparea))
-    FAOyields     <- ifelse(LUHcroparea[,,croplist]>0, FAOproduction[,,croplist] / LUHcroparea[,,croplist], 0)
-    # global mean
-    FAOyieldsGLO  <- dimSums(FAOyields, dim=1) / length(getCells(FAOyields))
+    # LPJmL cellular production
+    LPJmL_production <- croparea * yields
+    # LPJmL total production (irrigated + rainfed)
+    LPJmL_production <- dimSums(LPJmL_production, dim="irrigation")
+    # LPJmL iso-country production
+    ##### Aggregate to iso-countries
+    ##### NOTE: ideally this should be handled by dimSums(x,dim=c("x","y")) as soon as this functionality is available #####
+    out <- NULL
+    for (i in unique(gsub(".*\\.", "", getCells(LPJmL_production)))) {
+      tmp           <- dimSums(LPJmL_production[i,,], dim=1)
+      getCells(tmp) <- i
+      out <- mbind(out, tmp)
+    }
+    LPJmL_production <- out
+    rm(out, tmp)
+    ##### NOTE: ideally this should be handled by dimSums(x,dim=c("x","y")) as soon as this functionality is available #####
+    LPJmL_production <- toolCountryFill(LPJmL_production)
+    LPJmL_production[is.na(LPJmL_production)] <- 0
+    # Note: Following countries not part of 67k cells: "ABW" "AND" "ATA" "BES" "BLM" "BVT" "GIB" "LIE" "MAC" "MAF" "MCO" "SMR" "SXM" "VAT" "VGB"
 
-    # calibration factor: country-specific management intensity
-    Calib         <- FAOyields / FAOyieldsGLO
+    # FAO iso-country production
+    FAO_production   <- setYears(collapseNames(calcOutput("FAOmassbalance_pre", aggregate=FALSE)[,,"production"][,iniyear,"dm"]), NULL)
+    # list of commons crops
+    common_crops     <- intersect(getNames(FAO_production), getNames(LPJmL_production))
+    # adjust dimensions
+    tmp              <- LPJmL_production[,,common_crops]
+    tmp[,,]          <- 1
+    FAO_production   <- FAO_production[,,common_crops] * tmp[,,common_crops]
+
+    # Calibration Factor:
+    Calib            <- FAO_production[,,common_crops] / LPJmL_production[,,common_crops]
+    Calib[LPJmL_production[,,common_crops]==0] <- 0
+    ##### CORRECT!!! Seems very high...
 
     # add crops that are missing in FAO
-    missingcrops  <- setdiff(getNames(LUHcroparea), getNames(FAOproduction))
+    missingcrops  <- setdiff(croplist, common_crops)
     Calib         <- add_columns(Calib, addnm=missingcrops, dim=3.1)
     Calib[,,missingcrops]     <- 0
     names(dimnames(Calib))[1] <- "iso"
 
-    croplist      <- intersect(getNames(Calib), croplist)
     yield_gain    <- yield_gain[,,croplist] * Calib[intersect(unique(gsub(".*\\.", "", getCells(yield_gain))), getCells(Calib)),,croplist]
   }
 
@@ -74,16 +110,9 @@ calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, monetary, ini
 
     # share of corp area by crop type
     if (length(proxycrop)==1 && proxycrop=="historical") {
-      # historical crop mix
-      # read in total (irrigated + rainfed) croparea
-      croparea <- calcOutput("Croparea", years=iniyear, sectoral="kcr", cells="lpjcell", physical=TRUE, cellular=TRUE, irrigation=FALSE, aggregate=FALSE)
 
-      #### adjust cell name (until 67k cell names fully integrated in calcCroparea and calcLUH2v2!!!) ####
-      map                          <- toolGetMappingCoord2Country()
-      getCells(croparea)           <- paste(map$coords, map$iso, sep=".")
-      names(dimnames(croparea))[1] <- "x.y.iso"
-      #### adjust cell name (until 67k cell names fully integrated in calcCroparea and calcLUH2v2!!!) ####
-
+      # total croparea (irrigated + rainfed)
+      croparea     <- dimSums(croparea, dim="irrigation")
       # historical share of crop types in total cropland per cell
       croparea_shr <- croparea / dimSums(croparea, dim=3)
       # correct NAs: where no current cropland available -> representative crops (maize, rapeseed, pulses) assumed as proxy
