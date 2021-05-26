2708d3faa8f5488e5c647642bdd52c2f7e236017
Felicitas <beier@pik-potsdam.de>
2021-05-26 20:41:48 +0200

    correct function call of yield in calcYieldsAdjusted

--- R/calcYieldsAdjusted.R
+++ R/calcYieldsAdjusted.R
@@ -21,7 +21,7 @@
 calcYieldsAdjusted <- function(lpjml, climatetype, iniyear, selectyears, FAOyieldcalib) {
 
   # read in cellular lpjml yields [in tons/ha]
-  yields     <- setYears(calcOutput("Yields", source=c(lpjml=as.vector(lpjml["crop"]), isimip=NULL), cells="lpjcell", climatetype=climatetype, years=selectyears, aggregate=FALSE), selectyears)
+  yields     <- setYears(calcOutput("Yields", source=c(lpjml=lpjml[["crop"]], isimip=NULL), cells="lpjcell", climatetype=climatetype, years=selectyears, aggregate=FALSE), selectyears)
 
   # only crops (pasture is not irrigated)
   yields     <- yields[,,"pasture",invert=T]
