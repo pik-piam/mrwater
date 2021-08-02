fffbc39d341432c33032fafac3d926b0a3e445e4
Felicitas <beier@pik-potsdam.de>
2021-08-02 19:30:35 +0200

    bugfix in correction of section with unit == USD_m3 (related to new dimensionality of yields due to MC implementation)

--- R/calcIrrigYieldImprovementPotential.R
+++ R/calcIrrigYieldImprovementPotential.R
@@ -36,8 +36,8 @@ calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, unit,
                                                iniyear, selectyears, cropmix, yieldcalib, multicropping) {
 
   # yield reduction parameters (share of yield potential that can be reached in second / third season):
-  yld_shr_season2 <- 0.8
-  yld_shr_season3 <- 0.7
+  yldShr2 <- 0.8
+  yldShr3 <- 0.7
 
   # read in cellular lpjml yields [in tons/ha]
   yields   <- calcOutput("YieldsAdjusted", lpjml = lpjml, climatetype = climatetype,
@@ -73,19 +73,29 @@ calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, unit,
     # Read in irrigation water requirements (withdrawals) for all crops
     # (in m^3 per hectare per year) [smoothed and harmonized]
     # Note: users would pay for consumption rather than withdrawals [D'Odorico et al. (2020)]
-    irrig_withdrawal <- calcOutput("IrrigWatRequirements", lpjml = lpjml,
+    irrigReqWW <- calcOutput("IrrigWatRequirements", lpjml = lpjml,
                                    climatetype = climatetype, selectyears = selectyears, aggregate = FALSE)
-    irrig_withdrawal <- collapseNames(irrig_withdrawal[, , "withdrawal"])
-    irrig_withdrawal <- irrig_withdrawal[, , intersect(gsub("[.].*", "", getNames(irrig_withdrawal)), croplist)]
+    irrigReqWW <- collapseNames(irrigReqWW[, , "withdrawal"])
+    irrigReqWW <- irrigReqWW[, , intersect(gsub("[.].*", "", getNames(irrigReqWW)), croplist)]
 
     # Read in irrigation system area initialization
-    irrigation_system <- calcOutput("IrrigationSystem", source = "Jaegermeyr", aggregate = FALSE)
+    irrigationSystem <- calcOutput("IrrigationSystem", source = "Jaegermeyr", aggregate = FALSE)
 
     # Calculate irrigation water requirements
-    IWR  <- dimSums((irrigation_system[, , ] * irrig_withdrawal[, , ]), dim = 3.1)
+    IWR  <- dimSums((irrigationSystem[, , ] * irrigReqWW[, , ]), dim = 3.1)
     IWR  <- IWR[, , getNames(yields)]
 
-    unit <- "USD05 per m^3"
+    # Correction of small IWR: where < 10 m^3/ha (= 1mm = 1 l/m^2 = 10 m^3/ha): 0
+    IWR[IWR < 10] <- 10
+    # Correction of very small yields: where < 10 USD/ha: 0
+    tmp <- calcOutput("IrrigYieldImprovementPotential", unit = "USD_ha",
+                      lpjml = lpjml, climatetype = climatetype, cropmix = cropmix,
+                      iniyear = iniyear, selectyears = selectyears, yieldcalib = yieldcalib,
+                      multicropping = multicropping)
+    IWR[dimSums(tmp, dim = 3) < 10] <- 0
+    # Yields in USD/m^3
+    yields <- ifelse(IWR > 0, yields / IWR, 0)
+    unit   <- "USD05 per m^3"
 
   }
 
@@ -164,44 +174,44 @@ calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, unit,
     i3r3[mc[, , "irrigated"] == 3 & mc[, , "rainfed"] == 3] <- 1
 
     # additional seasonal gains
-    yield_gain_add <- new.magpie(cells_and_regions = getCells(yields),
+    yieldGainAdd <- new.magpie(cells_and_regions = getCells(yields),
                                  years = getYears(yields),
                                  names = c("ss", "sd", "st", "ds", "dd", "dt", "ts", "td", "tt"),
                                  fill = 0)
 
-    yield_gain_add[, , "ss"] <- collapseNames(yields[, , "irrigated"]) - collapseNames(yields[, , "rainfed"])
-    yield_gain_add[, , "ds"] <- yld_shr_season2 * collapseNames(yields[, , "irrigated"])
-    yield_gain_add[, , "dd"] <- yld_shr_season2 * (collapseNames(yields[, , "irrigated"]) -
+    yieldGainAdd[, , "ss"] <- collapseNames(yields[, , "irrigated"]) - collapseNames(yields[, , "rainfed"])
+    yieldGainAdd[, , "ds"] <- yldShr2 * collapseNames(yields[, , "irrigated"])
+    yieldGainAdd[, , "dd"] <- yldShr2 * (collapseNames(yields[, , "irrigated"]) -
                                                      collapseNames(yields[, , "rainfed"]))
-    yield_gain_add[, , "ts"] <- yld_shr_season3 * collapseNames(yields[, , "irrigated"])
-    yield_gain_add[, , "td"] <- yld_shr_season3 * collapseNames(yields[, , "irrigated"])
-    yield_gain_add[, , "tt"] <- yld_shr_season3 * (collapseNames(yields[, , "irrigated"]) -
+    yieldGainAdd[, , "ts"] <- yldShr3 * collapseNames(yields[, , "irrigated"])
+    yieldGainAdd[, , "td"] <- yldShr3 * collapseNames(yields[, , "irrigated"])
+    yieldGainAdd[, , "tt"] <- yldShr3 * (collapseNames(yields[, , "irrigated"]) -
                                                      collapseNames(yields[, , "rainfed"]))
 
     # report seasonal gains for three seasons
-    yield_gain <- new.magpie(cells_and_regions = getCells(yields),
+    yieldGain <- new.magpie(cells_and_regions = getCells(yields),
                              years = getYears(yields),
                              names = c("single", "double", "triple"),
                              fill = 0)
 
     if (multicropping) {
 
-      yield_gain[, , "single"] <- collapseNames(yield_gain_add[, , "ss"])
-      yield_gain[, , "single"] <- yield_gain[, , "single"] * (1 - i1r2) * (1 - i1r3)
+      yieldGain[, , "single"] <- collapseNames(yieldGainAdd[, , "ss"])
+      yieldGain[, , "single"] <- yieldGain[, , "single"] * (1 - i1r2) * (1 - i1r3)
 
-      yield_gain[, , "double"] <- collapseNames(yield_gain_add[, , "ds"]) * i2r1 +
-                                  collapseNames(yield_gain_add[, , "dd"]) * i2r2 +
-                                  collapseNames(yield_gain_add[, , "dt"]) * i2r3
+      yieldGain[, , "double"] <- collapseNames(yieldGainAdd[, , "ds"]) * i2r1 +
+                                  collapseNames(yieldGainAdd[, , "dd"]) * i2r2 +
+                                  collapseNames(yieldGainAdd[, , "dt"]) * i2r3
 
-      yield_gain[, , "triple"] <- collapseNames(yield_gain_add[, , "ts"]) * i3r1 +
-                                  collapseNames(yield_gain_add[, , "td"]) * i3r2 +
-                                  collapseNames(yield_gain_add[, , "tt"]) * i3r3
+      yieldGain[, , "triple"] <- collapseNames(yieldGainAdd[, , "ts"]) * i3r1 +
+                                  collapseNames(yieldGainAdd[, , "td"]) * i3r2 +
+                                  collapseNames(yieldGainAdd[, , "tt"]) * i3r3
 
     } else {
 
       # yield gain through irrigation for each crop [in tons/ha]
-      yield_gain[, , "single"] <- collapseNames(yield_gain_add[, , "ss"])
-      yield_gain[, , c("double", "triple")] <- 0
+      yieldGain[, , "single"] <- collapseNames(yieldGainAdd[, , "ss"])
+      yieldGain[, , c("double", "triple")] <- 0
 
     }
 
@@ -209,7 +219,7 @@ calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, unit,
 
     if (!multicropping) {
 
-      yield_gain  <- collapseNames(yields[, , "irrigated"]) - collapseNames(yields[, , "rainfed"])
+      yieldGain  <- collapseNames(yields[, , "irrigated"]) - collapseNames(yields[, , "rainfed"])
       description <- "Yield improvement potential by irrigation for all different crop types"
 
     } else {
@@ -219,39 +229,24 @@ calcIrrigYieldImprovementPotential <- function(lpjml, climatetype, unit,
   }
 
   # set negative yield gains to 0
-  yield_gain[yield_gain < 0] <- 0
+  yieldGain[yieldGain < 0] <- 0
   # (Note: irrigation may lead to shift in growing period -> can have negative values
   # (in cropping calendar version);
   # also: under N-stress, irrigation may lead to lower yields,
   # (the latter is only relevant for limited-N-LPJmL version, default: unlimited N))
 
-  if (unit == "USD_m3") {
-
-    # Correction of small yield gains and small IWR: where smaller (a) or (b) set to 0
-    # (a) 1 mm = l/m^2 = 10 m^3/ha
-    # (b) 10 USD /ha
-    setToZero <- IWR < 10 | yield_gain < 10
-
-    IWR[setToZero]        <- 0
-    yield_gain[setToZero] <- 0
-
-    # Calculate value of water (yield improvement per m^3)
-    yield_gain <- ifelse(IWR > 0, yield_gain / IWR, 0)
-
-  }
-
   # Check for NAs
-  if (any(is.na(yield_gain))) {
+  if (any(is.na(yieldGain))) {
     stop("Function YieldImprovementPotential produced NAs")
   }
 
   # Check for negatives
-  if (any(round(yield_gain) < 0)) {
+  if (any(round(yieldGain) < 0)) {
     stop("Function YieldImprovementPotential produced negative values")
   }
 
 
-  return(list(x            = yield_gain,
+  return(list(x            = yieldGain,
               weight       = NULL,
               unit         = unit,
               description  = description,
