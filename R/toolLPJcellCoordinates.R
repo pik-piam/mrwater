93f047c62821d3f84cece586f96ce64e220da50e
Felicitas <beier@pik-potsdam.de>
2021-12-15 18:02:34 +0100

    included LPJ_CellBelongingsToCountries.csv in mrcommons rather than mappingfolder

--- R/toolLPJcellCoordinates.R
+++ R/toolLPJcellCoordinates.R
@@ -16,26 +16,31 @@
 toolLPJcellCoordinates <- function(x, type) {
 
   # read in LPJmL mapping
-  LPJcells <- toolGetMapping("LPJ_CellBelongingsToCountries.csv", type="cell", where="mappingfolder")
+  lpjCells <- toolGetMapping("LPJ_CellBelongingsToCountries.csv", where = "mcrommons")
   # transform to coordinate style data with p separation
-  LPJcells <- data.frame(LPJcells, data.frame("coordinates"=paste(gsub("\\.", "p", LPJcells$lon), gsub("\\.", "p", LPJcells$lat), sep="."),stringsAsFactors=F))
+  lpjCells <- data.frame(lpjCells, data.frame("coordinates" = paste(gsub("\\.", "p", lpjCells$lon),
+                                                                    gsub("\\.", "p", lpjCells$lat), sep = "."),
+                                              stringsAsFactors = FALSE))
   # add cell number (LPJmL cell code)
-  LPJcells <- data.frame(LPJcells, data.frame("cell"=paste(LPJcells$ISO,c(1:67420),sep="."),stringsAsFactors=F))
+  lpjCells <- data.frame(lpjCells, data.frame("cell" = paste(lpjCells$ISO, c(1:67420), sep = "."),
+                                              stringsAsFactors = FALSE))
   # subset data frame
-  LPJcells <- data.frame("cell"=LPJcells$cell,"coordinates"=LPJcells$coordinates,stringsAsFactors=F)
+  lpjCells <- data.frame("cell" = lpjCells$cell,
+                         "coordinates" = lpjCells$coordinates,
+                         stringsAsFactors = FALSE)
 
 
-  if (type=="coord2lpj") {
+  if (type == "coord2lpj") {
 
     # sort cells
-    out <- x[LPJcells$coordinates,,]
+    out <- x[lpjCells$coordinates, , ]
     # rename
-    getCells(out) <- LPJcells$cell
+    getCells(out) <- lpjCells$cell
 
-  } else if (type=="lpj2coord") {
+  } else if (type == "lpj2coord") {
 
-    # transform lpjcells to coordinates
-    getCells(x) <- LPJcells$coordinates
+    # transform lpjCells to coordinates
+    getCells(x) <- lpjCells$coordinates
     out <- x
 
   } else {
