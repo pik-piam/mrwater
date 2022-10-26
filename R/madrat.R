49a317550c7dcb7c99243e77c9079f225f3e5464
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-10-26 14:44:04 +0200

    adjusted madrat.R

--- R/madrat.R
+++ R/madrat.R
@@ -1,4 +1,3 @@
-#' @importFrom madrat vcat
 
 .onAttach <- function(libname, pkgname) {
   madrat::madratAttach(pkgname)
@@ -7,9 +6,3 @@
 .onDetach <- function(libpath) {
   madrat::madratDetach(libpath)
 }
-
-# redirect standard messaging functions to vcat
-cat     <- function(...) vcat(1, ...)
-message <- function(...) vcat(1, ...)
-warning <- function(...) vcat(0, ...)
-stop    <- function(...) vcat(-1, ...)
