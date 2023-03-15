adb4964a2f452730edd848dfa7a2bd3db4f4907e
Kristine Karstens <karstens@pik-potsdam.de>
2023-03-15 16:09:21 +0100

    bugfix named list for lpjml version

--- R/calcGrowingPeriod.R
+++ R/calcGrowingPeriod.R
@@ -27,11 +27,11 @@ calcGrowingPeriod <- function(lpjml = c(natveg = "LPJmL4_for_MAgPIE_44ac93de", c
   cfgNatveg <- toolLPJmLVersion(version = lpjml["natveg"], climatetype = climatetype)
   cfgCrop   <- toolLPJmLVersion(version = lpjml["crop"],   climatetype = climatetype)
 
-  lpjmlReadin  <- c(natveg = cfgNatveg$readin_version,
-                    crop   = cfgCrop$readin_version)
+  lpjmlReadin  <- c(natveg = unname(cfgNatveg$readin_version),
+                    crop   = unname(cfgCrop$readin_version))
 
-  lpjmlBaseline <- c(natveg = cfgNatveg$baseline_version,
-                      crop  = cfgCrop$baseline_version)
+  lpjmlBaseline <- c(natveg = unname(cfgNatveg$baseline_version),
+                      crop  = unname(cfgCrop$baseline_version))
 
   if (stage %in% c("raw", "smoothed")) {
 
